6cb8580c63319275620ee7aa432b387085ae752a
Jerry Jigarius Radwick <r2d2@jigarius.com>
2024-08-06 00:05:56 +0530

    Replace ConfigHelper with ConfigBuilder

--- dist/gulp.config.mjs
+++ dist/gulp.config.mjs
@@ -4,18 +4,18 @@
  * This config file is for use during the development of drupal-gulp.
  */
 
-import { ConfigHelper } from 'drupal-gulp';
+import { ConfigBuilder } from 'drupal-gulp';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
 const cwd = path.dirname(fileURLToPath(import.meta.url));
-const ch = new ConfigHelper(cwd);
+const builder = new ConfigBuilder(cwd);
 
-let config = ch.getDefaults();
+builder.applyDefaults();
 
-// Modify configuration, as required.
-// config.styleSources.push('foo/**/*.scss')
-// Alternatively, build your own Config object (not recommended).
-// config = new Config();
+// Modify configuration through the builder as required.
+// builder
+//   .addAllCustomModules()
+//   .addAllCustomThemes();
 
-export default config;
+export default builder.build();
